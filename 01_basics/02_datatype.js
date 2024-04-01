385d08315b9df31dc90871a3e840151142d967b8
Sumit Prasad Gupta <145041771+Sumit8617@users.noreply.github.com>
2024-04-01 10:05:20 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict";   //treat all JS code as a newer version
+
+// alert(3+3)  //we ar using nodejs not browser
+
+console.log(3+3)
+console.log("Sumit")
+
+let name="Sumit Prasad Gupta"
+let age= 20
+let isLoggedIn=false
+let stage;   //undefined 
+let abc=null  //means this is an empty variable
+
+//number=> 2 to the power 53
+//bigint=
+//string= " "
+//boolean => true/false
+//null => standalone value
+//symbol => uniqueness
+
+//object
+
+console.log(typeof null)          //object
+console.log(typeof undefined)     //undefined
